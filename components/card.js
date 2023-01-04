6a5f4e1bbefbe301a62293b8c4caad14bb5dcd4e
haidoe <haidren.amalia@gmail.com>
2023-01-03 23:50:56 -0800

    Card styling initial update

--- components/card.js
+++ components/card.js
@@ -1,11 +1,24 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from "./card.module.css";
 
 const Card = (props) => {
   return (
-    <Link href={props.href}>
-      <h2>{props.storeName}</h2>
-      <Image src={props.imgUrl} width={250} height={150} alt="Coffee Image" />
+    <Link href={props.href} style={styles.cardLink}>
+      <div className={styles.container}>
+        <div className={styles.cardHeaderWrapper}>
+          <h2 className={styles.cardHeader}>{props.storeName}</h2>
+        </div>
+
+        <div className={styles.cardImageWrapper}>
+          <Image
+            src={props.imgUrl}
+            width={250}
+            height={150}
+            alt="Coffee Image"
+          />
+        </div>
+      </div>
     </Link>
   );
 };
